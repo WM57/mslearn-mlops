6bb8266df5bf3d15b8121a1c16ff9336dc079dd9
WM57 <walid.marouf@gmail.com>
2024-03-25 16:27:58 +0100

    ok

--- src/model/train.py
+++ src/model/train.py
@@ -10,6 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 import mlflow
+
 # define functions
 
 
@@ -18,33 +19,53 @@ def main(args):
 
     mlflow.autolog()
     # read data
-    df = get_csvs_df(args.training_data)
+    df = get_csvs_df(
+        args.training_data)
 
     # split data
-    X_train, X_test, y_train, y_test = split_data(df)
+    X_train, X_test, y_train, y_test = split_data(
+        df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    train_model(
+        args.
+        reg_rate,
+        X_train,
+        X_test,
+        y_train,
+        y_test)
 
 
-def get_csvs_df(path):
+def get_csvs_df(
+        path):
     if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
+        raise RuntimeError(
+            f"Cannot use non-existent path provided:{path}")
+    csv_files = glob.glob(
+        f"{path}/*.csv")
     if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+        raise RuntimeError(
+            f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
-               'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose',
+               'DiastolicBloodPressure', 'TricepsThickness',
+               'SerumInsulin', 'BMI',
+               'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
+def train_model(
+        reg_rate,
+        X_train,
+        X_test,
+        y_train,
+        y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(
+        C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
@@ -52,8 +73,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest="training_data", default="experimentation/data/",
-                        type=str)
+    parser.add_argument("--training_data", dest="training_data",
+                        default="experimentation/data/", type=str)
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.02)
 
@@ -70,11 +91,15 @@ if __name__ == "__main__":
     print("\n\n")
     print("*" * 60)
 
-    print(os.getcwd())
+    print(
+        os.getcwd())
 
     # parse args
     args = parse_args()
-    print("ARGS", args, type(args))
+    print(
+        "ARGS",
+        args,
+        type(args))
     # run main function
     main(args)
 
